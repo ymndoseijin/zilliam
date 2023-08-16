a3c7741616afbe08a37d9fce5993dd712c47eaaa
Kauê 地球の土星人 <kauecmf@protonmail.com>
2023-08-15 23:28:19 -0300

    many things

--- src/geo.zig
+++ src/geo.zig
@@ -24,59 +24,6 @@ fn basisRecursion(comptime dim: usize, start: usize, num: usize, length: usize, 
     }
 }
 
-pub fn getMultivectorEnum(comptime dim: usize) type {
-    const basis_num = (2 << (dim - 1)) - 1;
-
-    const Data = struct {
-        tags: [dim]usize,
-        count: usize,
-    };
-
-    var index: usize = 0;
-    var indices: [basis_num]Data = undefined;
-
-    for (1..dim + 1) |length| {
-        for (1..dim + 1) |i| {
-            var buff: [dim]usize = .{0} ** dim;
-            buff[0] = i;
-            basisRecursion(
-                dim,
-                i,
-                0,
-                length,
-                &indices,
-                &index,
-                buff,
-            );
-        }
-    }
-
-    var fields: [basis_num]std.builtin.Type.EnumField = undefined;
-    for (indices, 0..) |basis, i| {
-        var buff: [dim * basis_num]u8 = undefined;
-        var fba = std.heap.FixedBufferAllocator.init(&buff);
-        var alloc = fba.allocator();
-        var name = std.ArrayList(u8).init(alloc);
-        var writer = name.writer();
-        _ = writer.write("e") catch unreachable;
-        for (basis.tags[0..basis.count]) |val| {
-            _ = writer.print("{}", .{val}) catch unreachable;
-        }
-        fields[i] = .{ .name = name.items, .value = i };
-    }
-
-    return @Type(.{
-        .Enum = .{
-            .tag_type = @Type(.{
-                .Int = .{ .signedness = .unsigned, .bits = basis_num },
-            }),
-            .fields = &fields,
-            .decls = &.{},
-            .is_exhaustive = false,
-        },
-    });
-}
-
 pub fn getBasis(comptime R: type, comptime dim: usize) R {
     var index: usize = 0;
     var indices: R = undefined;
@@ -97,20 +44,22 @@ pub fn getBasis(comptime R: type, comptime dim: usize) R {
     }
     return indices;
 }
-pub fn Algebra(comptime T: type, comptime dim: usize) type {
-    const basis_num = (2 << (dim - 1)) - 1;
+pub fn Algebra(comptime T: type, comptime pos_dim: usize, comptime null_dim: usize) type {
+    const sum_of_dim = pos_dim + null_dim;
+    const basis_num = (2 << (sum_of_dim - 1)) - 1;
 
     const Data = struct {
-        tags: [dim]usize,
+        // .{n_1, ..., n_{neg_dim}, z_1, ..., z_{null_dim}, p_1, ..., p_{pos_dim}, scalar}
+        tags: [sum_of_dim]usize,
         count: usize,
     };
 
     const indices: [basis_num + 1]Data = blk: {
         var temp: [basis_num + 1]Data = undefined;
-        for (getBasis([basis_num]Data, dim), 0..) |basis, i| {
-            temp[i] = basis;
+        temp[0] = .{ .tags = .{}, .count = 0 };
+        for (getBasis([basis_num]Data, sum_of_dim), 0..) |basis, i| {
+            temp[i + 1] = basis;
         }
-        temp[basis_num] = .{ .tags = .{}, .count = 0 };
         break :blk temp;
     };
 
@@ -121,8 +70,8 @@ pub fn Algebra(comptime T: type, comptime dim: usize) type {
 
         pub const Inputs = blk: {
             var fields: [basis_num]std.builtin.Type.StructField = undefined;
-            for (indices[0..basis_num], 0..) |basis, i| {
-                var buff: [dim * basis_num + 1024]u8 = undefined;
+            for (indices[1 .. basis_num + 1], 0..) |basis, i| {
+                var buff: [sum_of_dim * basis_num + 1024]u8 = undefined;
                 var fba = std.heap.FixedBufferAllocator.init(&buff);
                 var alloc = fba.allocator();
                 var name = std.ArrayList(u8).init(alloc);
@@ -131,10 +80,13 @@ pub fn Algebra(comptime T: type, comptime dim: usize) type {
                 _ = writer.write("e") catch unreachable;
 
                 for (basis.tags[0..basis.count]) |val| {
-                    _ = writer.print("{}", .{val}) catch unreachable;
+                    var actual: i32 = @intCast(val);
+                    actual -= null_dim;
+                    _ = writer.print("{}", .{actual}) catch unreachable;
                 }
                 var res = Self{};
-                res.val[i] = 1;
+                res.val[i + 1] = 1;
+
                 fields[i] = .{
                     .name = name.items,
                     .type = Self,
@@ -156,7 +108,7 @@ pub fn Algebra(comptime T: type, comptime dim: usize) type {
 
         pub const geoCtx = simpleCtx(struct {
             pub const UnOp = enum { @"-" };
-            pub const BinOp = enum { @"+", @"-", @"*" };
+            pub const BinOp = enum { @"+", @"-", @"*", @"^" };
 
             pub const allow_unused_inputs = true;
 
@@ -164,6 +116,7 @@ pub fn Algebra(comptime T: type, comptime dim: usize) type {
                 .@"+" = .{ .prec = 10, .assoc = .left },
                 .@"-" = .{ .prec = 10, .assoc = .left },
                 .@"*" = .{ .prec = 20, .assoc = .left },
+                .@"^" = .{ .prec = 20, .assoc = .left },
             };
 
             pub fn EvalUnOp(comptime op: []const u8, comptime U: type) type {
@@ -195,6 +148,7 @@ pub fn Algebra(comptime T: type, comptime dim: usize) type {
                     .@"+" => lhs.add(rhs),
                     .@"-" => lhs.sub(rhs),
                     .@"*" => lhs.mul(rhs),
+                    .@"^" => lhs.wedge(rhs),
                 };
             }
         }{});
@@ -205,7 +159,7 @@ pub fn Algebra(comptime T: type, comptime dim: usize) type {
 
         pub fn fromInt(num: anytype) Self {
             var res = Self{};
-            res.val[basis_num] = num;
+            res.val[0] = num;
             return res;
         }
 
@@ -215,13 +169,13 @@ pub fn Algebra(comptime T: type, comptime dim: usize) type {
             neg,
         };
 
-        inline fn multiplyBasisWithSingle(i: usize, b_dim: usize) struct { usize, Sign } {
+        inline fn multiplyBasisWithSingle(comptime quadratic_form: Sign, i: usize, b_dim: usize) struct { usize, Sign } {
             // if it's a constant
-            if (i == basis_num) return .{ b_dim - 1, .pos };
+            if (i == 0) return .{ b_dim, .pos };
 
             const a = indices[i];
 
-            var result: Data = .{ .tags = .{0} ** dim, .count = 0 };
+            var result: Data = .{ .tags = .{0} ** sum_of_dim, .count = 0 };
 
             var swap: bool = false;
 
@@ -234,7 +188,7 @@ pub fn Algebra(comptime T: type, comptime dim: usize) type {
                     var index: usize = 0;
 
                     if (reversed > b_dim) {
-                        if (a.count != dim) {
+                        if (a.count != sum_of_dim) {
                             for (0..pos) |idx| {
                                 result.tags[index] = a.tags[idx];
                                 index += 1;
@@ -272,6 +226,17 @@ pub fn Algebra(comptime T: type, comptime dim: usize) type {
                         result.tags[index] = a.tags[idx];
                         index += 1;
                     }
+
+                    if (null_dim >= b_dim and b_dim > 0) {
+                        return .{ b_dim, .zero };
+                    }
+
+                    switch (quadratic_form) {
+                        .zero => return .{ b_dim, .zero },
+                        .neg => swap = !swap,
+                        .pos => {},
+                    }
+
                     for (pos + 1..a_len) |idx| {
                         result.tags[index] = a.tags[idx];
                         index += 1;
@@ -291,21 +256,22 @@ pub fn Algebra(comptime T: type, comptime dim: usize) type {
             @panic("FAIL!");
         }
 
-        pub fn multiplyBasis(i: usize, j: usize) struct { usize, Sign } {
+        pub fn multiplyBasis(comptime quadratic_form: Sign, i: usize, j: usize) struct { usize, Sign } {
             const b = indices[j];
             var r = i;
             var sign: Sign = .pos;
             for (b.tags[0..b.count]) |tag| {
-                const res = multiplyBasisWithSingle(r, tag);
-                if (res[1] == .zero) return .{ basis_num - 1, .zero };
+                const res = multiplyBasisWithSingle(quadratic_form, r, tag);
+
+                if (res[1] == .zero) return .{ 0, .zero };
+                sign = if (res[1] == sign) .pos else .neg;
 
                 r = res[0];
-                sign = if (res[1] == sign) .pos else .neg;
             }
             return .{ r, sign };
         }
 
-        pub fn mul(a: Self, b: Self) Self {
+        pub fn anticommute(a: Self, comptime quadratic_form: Sign, b: Self) Self {
             var r: Self = undefined;
             var vec: @Vector(basis_num + 1, T) = .{0} ** (basis_num + 1);
             for (a.val, 0..) |a_us, a_i| {
@@ -313,15 +279,12 @@ pub fn Algebra(comptime T: type, comptime dim: usize) type {
                 for (b.val, 0..) |b_us, b_i| {
                     if (b_us == 0) continue;
 
-                    const res = multiplyBasis(a_i, b_i);
+                    const res = multiplyBasis(quadratic_form, a_i, b_i);
 
                     const sign: T = switch (res[1]) {
                         .pos => 1,
                         .neg => -1,
-                        .zero => {
-                            r.val = .{0} ** (basis_num + 1);
-                            return r;
-                        },
+                        .zero => 0,
                     };
                     const a_scalar: T = @intCast(a_us);
                     const b_scalar: T = @intCast(b_us);
@@ -332,6 +295,36 @@ pub fn Algebra(comptime T: type, comptime dim: usize) type {
             r.val = vec;
 
             return r;
+        }
+
+        pub fn mul(a: Self, b: Self) Self {
+            return a.anticommute(.pos, b);
+        }
+
+        pub fn wedge(a: Self, b: Self) Self {
+            return a.anticommute(.zero, b);
+        }
+
+        pub fn hodge(a: Self) Self {
+            var r: Self = undefined;
+
+            for (a.val, 0..) |scalar, i| {
+                const res = multiplyBasis(.pos, i, basis_num - i);
+
+                const sign: T = switch (res[1]) {
+                    .pos => 1,
+                    .neg => -1,
+                    .zero => 0,
+                };
+
+                r.val[basis_num - i] = sign * scalar;
+            }
+            return r;
+        }
+
+        // ?
+        pub fn rotor(plane: Self, theta: T) Self {
+            return Self.fromInt(std.math.cos(theta)).add(plane.mul(std.math.sin(theta)));
         }
 
         pub fn add(a: Self, b: Self) Self {
@@ -357,69 +350,100 @@ pub fn Algebra(comptime T: type, comptime dim: usize) type {
 }
 
 test "algebra" {
-    inline for (2..5) |i| {
-        const Alg = Algebra(i32, i);
+    const Alg = Algebra(i32, 2, 1);
 
-        try std.testing.expectEqualSlices(
-            i32,
-            &(try Alg.evalBasis("(3*e12+2*e1)*(2*e2)")).val,
-            &(try Alg.evalBasis("6*e1+4*e12")).val,
-        );
+    try std.testing.expectEqualSlices(
+        i32,
+        &(try Alg.evalBasis("(3*e12+2*e1)*(2*e2)")).val,
+        &(try Alg.evalBasis("6*e1+4*e12")).val,
+    );
 
-        try std.testing.expectEqualSlices(
-            i32,
-            &(try Alg.evalBasis("(3*e12+2*e1)*(2*e2+1)")).val,
-            &(try Alg.evalBasis("8*e1+7*e12")).val,
-        );
+    try std.testing.expectEqualSlices(
+        i32,
+        &(try Alg.evalBasis("(3*e12+2*e1)*(2*e2+1)")).val,
+        &(try Alg.evalBasis("8*e1+7*e12")).val,
+    );
 
-        try std.testing.expectEqualSlices(
-            i32,
-            &(try Alg.evalBasis("(3*e12+2*e1)*e1")).val,
-            &(try Alg.evalBasis("-3*e2+2")).val,
-        );
+    try std.testing.expectEqualSlices(
+        i32,
+        &(try Alg.evalBasis("(3*e12+2*e1)*e1")).val,
+        &(try Alg.evalBasis("-3*e2+2")).val,
+    );
 
-        // 6*e1-3*e2+4*e12+2 = 6*e1-3*e2-4*e12-2???
-        // temp: "6*e1-(3*e2)+4*e12+2 seems to fix it (it's a comath bug)
+    // 6*e1-3*e2+4*e12+2 = 6*e1-3*e2-4*e12-2???
+    // temp: "6*e1-(3*e2)+4*e12+2 seems to fix it (it's a comath bug)
 
-        try std.testing.expectEqualSlices(
-            i32,
-            &(try Alg.evalBasis("(2*e1+3*e12)*(e1+2*e2)")).val,
-            &(try Alg.evalBasis("6*e1-(3*e2)+4*e12+2")).val,
-        );
+    try std.testing.expectEqualSlices(
+        i32,
+        &(try Alg.evalBasis("(2*e1+3*e12)*(e1+2*e2)")).val,
+        &(try Alg.evalBasis("6*e1-(3*e2)+4*e12+2")).val,
+    );
 
-        try std.testing.expectEqualSlices(
-            i32,
-            &Alg.fromInt(-15).val,
-            &(try Alg.evalBasis("(3*e12)*(5*e12)")).val,
-        );
+    try std.testing.expectEqualSlices(
+        i32,
+        &Alg.fromInt(-15).val,
+        &(try Alg.evalBasis("(3*e12)*(5*e12)")).val,
+    );
 
-        try std.testing.expectEqualSlices(
-            i32,
-            &(try Alg.evalBasis("(2*e1)*(5*e12)")).val,
-            &(try Alg.evalBasis("10*e2")).val,
-        );
+    try std.testing.expectEqualSlices(
+        i32,
+        &(try Alg.evalBasis("(2*e1)*(5*e12)")).val,
+        &(try Alg.evalBasis("10*e2")).val,
+    );
 
-        try std.testing.expectEqualSlices(
-            i32,
-            &(try Alg.evalBasis("(2*e1)*(5*e12)")).val,
-            &(try Alg.evalBasis("10*e2")).val,
-        );
+    try std.testing.expectEqualSlices(
+        i32,
+        &(try Alg.evalBasis("(2*e1)*(5*e12)")).val,
+        &(try Alg.evalBasis("10*e2")).val,
+    );
 
-        try std.testing.expectEqualSlices(
-            i32,
-            &(try Alg.evalBasis("(14*e1) - (21*e2) - (7*e12) - 24")).val,
-            &(try Alg.evalBasis("(2*e1+3*e2+5*e12) * (11*e1+13*e2+17*e12)")).val,
-        );
+    try std.testing.expectEqualSlices(
+        i32,
+        &(try Alg.evalBasis("(14*e1) - (21*e2) - (7*e12) - 24")).val,
+        &(try Alg.evalBasis("(2*e1+3*e2+5*e12) * (11*e1+13*e2+17*e12)")).val,
+    );
 
-        try std.testing.expectEqualSlices(
-            i32,
-            &(try Alg.evalBasis("129*e1 + 127*e2 + 207*e12 + 109")).val,
-            &(try Alg.evalBasis("(2*e1+3*e2+5*e12+7) * (11*e1+13*e2+17*e12+19)")).val,
-        );
-        try std.testing.expectEqualSlices(
-            i32,
-            &Alg.fromInt(-1).val,
-            &(try Alg.evalBasis("e12 * e12")).val,
-        );
-    }
+    try std.testing.expectEqualSlices(
+        i32,
+        &(try Alg.evalBasis("129*e1 + 127*e2 + 207*e12 + 109")).val,
+        &(try Alg.evalBasis("(2*e1+3*e2+5*e12+7) * (11*e1+13*e2+17*e12+19)")).val,
+    );
+
+    // wedge
+    try std.testing.expectEqualSlices(
+        i32,
+        &(try Alg.evalBasis("133 + (115*e1) + (148*e2) + (207*e12)")).val,
+        &(try Alg.evalBasis("(2*e1+3*e2+5*e12+7) ^ (11*e1+13*e2+17*e12+19)")).val,
+    );
+
+    try std.testing.expectEqualSlices(
+        i32,
+        &Alg.fromInt(0).val,
+        &(try Alg.evalBasis("(2*e1)^(5*e12)")).val,
+    );
+
+    // null
+    try std.testing.expectEqualSlices(
+        i32,
+        &Alg.fromInt(0).val,
+        &(try Alg.evalBasis("e0*e0")).val,
+    );
+
+    try std.testing.expectEqualSlices(
+        i32,
+        &Alg.fromInt(0).val,
+        &(try Alg.evalBasis("e0^e0")).val,
+    );
+
+    try std.testing.expectEqualSlices(
+        i32,
+        &(try Alg.evalBasis("654*e0 + 129*e1 + 127*e2 + 191*e01 + 206*e02 + 207*e12 + 546*e012 + 109")).val,
+        &(try Alg.evalBasis("(23*e0+2*e1+3*e2+5*e12+7) * (31*e0+11*e1+13*e2+17*e12+19)")).val,
+    );
+
+    try std.testing.expectEqualSlices(
+        i32,
+        &(try Alg.evalBasis("5*e0 + 3*e01 + -2*e02 + 11*e12 + 7*e012")).val,
+        &(try Alg.evalBasis("11*e0+2*e1+3*e2+5*e12+7")).hodge().val,
+    );
 }
